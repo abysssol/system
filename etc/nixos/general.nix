aab70b32d9410452ddfea76ee63e54b550bfdcff
Abysss <abysssol@pm.me>
2022-06-15 03:09:12 -0400

    Change from rtkit to wireplumber for pipewire

--- etc/nixos/general.nix
+++ etc/nixos/general.nix
@@ -11,8 +11,6 @@
       [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
   };
 
-  security.rtkit.enable = true;
-
   services = {
     emacs.enable = true;
     emacs.package = pkgs.emacsNativeComp;
@@ -24,6 +22,7 @@
       alsa.support32Bit = true;
       pulse.enable = true;
       jack.enable = true;
+      wireplumber.enable = true;
     };
 
     xserver = {
