9899db22be716607b789d661c28c1f5e6ea682c2
abysssol <abysssol@pm.me>
2023-03-04 10:48:05 -0500

    Make flake-utils a flake input

--- flake.nix
+++ flake.nix
@@ -3,21 +3,27 @@
     nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
+    utils.url = "github:numtide/flake-utils";
+
     rust = {
       url = "github:oxalica/rust-overlay";
-      inputs = { nixpkgs.follows = "nixpkgs-unstable"; };
+      inputs = {
+        nixpkgs.follows = "nixpkgs-unstable";
+        flake-utils.follows = "utils";
+      };
     };
 
     dmm = {
       url = "github:abysssol/dmm";
       inputs = {
         nixpkgs.follows = "nixpkgs-unstable";
+        utils.follows = "utils";
         rust.follows = "rust";
       };
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, rust, dmm, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, rust, dmm, ... }:
     let
       system = "x86_64-linux";
       hostname = "tungsten";
@@ -30,10 +36,11 @@
 
       defaultPackage = name: value: value.packages.${system}.default;
       flakes = pkgs.lib.attrsets.mapAttrs defaultPackage flakePkgs;
+
+      specialArgs = { inherit system hostname unstable flakes; };
     in {
       nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = inputs // { inherit system hostname unstable flakes; };
+        inherit system specialArgs;
         modules = [
           ./hardware-configuration.nix
           ./local-configuration.nix
